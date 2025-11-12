506003ca22870545979c2e55111262091248f2c0
Wakhanu-Satwinder <windersonrolles@gmail.com>
2025-11-12 22:33:55 +0300

    render file

--- sts/sts/settings.py
+++ sts/sts/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost",".vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
 
 # Application definition
