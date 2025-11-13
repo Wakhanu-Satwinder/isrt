9ceb65830c3f82ac320f5e380ced53ba24b4a6fa
Wakhanu-Satwinder <windersonrolles@gmail.com>
2025-11-13 11:38:39 +0300

    wsgi adjustment

--- sts/sts/settings.py
+++ sts/sts/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -201,7 +201,6 @@ SESSION_COOKIE_HTTPONLY=False
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://isrts.netlify.app/'
 ]
 CORS_ALLOW_ALL_ORIGINS=True
 '''CORS_ALLOW_CREDENTIALS=True
