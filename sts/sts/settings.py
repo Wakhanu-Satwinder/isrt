7a86da2c956c58b260e8e12b863d4a86d6ea0519
Wakhanu-Satwinder <windersonrolles@gmail.com>
2025-11-21 20:53:12 +0300

    adding base dir in settings

--- sts/sts/settings.py
+++ sts/sts/settings.py
@@ -9,6 +9,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 import dj_database_url
 from decouple import config
 
@@ -34,7 +37,7 @@ conn = psycopg2.connect(DATABASE_URL)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
